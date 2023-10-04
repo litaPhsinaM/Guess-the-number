3bbd5c4e3b705e07648e5553fa2013fe3be535dc
litaPhsinaM <manishcpatil9@gmail.com>
2023-10-03 18:56:56 -0700

    first edit

--- guessMyNo.js
+++ guessMyNo.js
@@ -44,7 +44,7 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector('.score').textContent = score;
-    displayMessage('Start Guessing...');
+    displayMessage('Start Guessing..');
     document.querySelector('.number').textContent = '?';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.guess').value = '';
